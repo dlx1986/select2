242eff3c0540476852e0ab4502842e4ddd89d040
ZhongYou <zhongyou.dlx@taobao.com>
2013-03-18 18:54:00 +0800

    Add popular items, show popular items when click.

--- select2_locale_zh-CN.js
+++ select2_locale_zh-CN.js
@@ -9,6 +9,7 @@
         formatInputTooLong: function (input, max) { var n = input.length - max; return "请删掉" + n + "个字符";},
         formatSelectionTooBig: function (limit) { return "你只能选择最多" + limit + "项"; },
         formatLoadMore: function (pageNumber) { return "加载结果中..."; },
-        formatSearching: function () { return "搜索中..."; }
+        formatSearching: function () { return "搜索中..."; },
+        formatSelectPopular: function (input, min) { var n = min - input.length; return "请再输入" + n + "个字符, 或选择以下常用项";}
     });
 })(jQuery);
